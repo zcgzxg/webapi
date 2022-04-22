ce76e1a3baa6f22812f5c7c8a03eb0cacaa523be
周瑜 <1134495285@qq.com>
2022-04-22 17:18:26 +0800

    init proj

---
+++ Models/Test.cs
@@ -0,0 +1,10 @@
+namespace webapi.Models
+{
+    public class Test
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public byte Age { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+using webapi.Middlewares;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Services.AddControllers();
+builder.Services.AddTransient<MySqlConnector.MySqlConnection>(_ => new MySqlConnector.MySqlConnection(builder.Configuration["ConnectionString:Default"]));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.UseNotfoundMiddleware();
+
+app.Run();
