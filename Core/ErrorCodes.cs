db92e11935a904bf4439d36ce67877592c632c99
周瑜 <1134495285@qq.com>
2022-05-11 17:15:56 +0800

    enum自定义名称

--- Core/ErrorCodes.cs
+++ Core/ErrorCodes.cs
@@ -1,24 +1,25 @@
-using System.ComponentModel.DataAnnotations;
 using System.Runtime.Serialization;
-using System.ComponentModel;
-using System.Text.Json.Serialization;
-using System.Text.Json;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace WebApi.Base
 {
     /// <summary>
     /// 返回给前端的错误代码
     /// </summary>
-    [JsonConverter(typeof(System.Text.Json.Serialization.JsonStringEnumConverter))]
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum ErrorCodes
     {
         /// <summary>
         /// 成功
         /// </summary>
+        [EnumMember(Value = "000001")]
         Success = 1,
         /// <summary>
         /// 出现错误
         /// </summary>
+        [EnumMember(Value = "000002")]
         Error = 2
     }
 }
