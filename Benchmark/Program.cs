73c8334d818712d6ea3860d5c37aa19549966f75
周瑜 <1134495285@qq.com>
2022-05-17 15:44:54 +0800

    修改Benchmark

--- Benchmark/Program.cs
+++ Benchmark/Program.cs
@@ -10,6 +10,7 @@ namespace ControllerBenchmark
     [RPlotExporter]
     [ThreadingDiagnoser]
     [MemoryDiagnoser]
+    [EventPipeProfiler(BenchmarkDotNet.Diagnosers.EventPipeProfile.GcCollect | BenchmarkDotNet.Diagnosers.EventPipeProfile.CpuSampling)]
     [IterationTime(120)]
     [IterationCount(10)]
     [InvocationCount(2)]
@@ -40,10 +41,16 @@ namespace ControllerBenchmark
             await BenchGetCategory(50);
         }
 
+        [Benchmark]
+        public async Task HighestBench()
+        {
+            await BenchGetCategory(80);
+        }
+
         public async Task BenchGetCategory(int parallelCount)
         {
             var tasks = new Task<WebApi.Base.CommonResponse<IEnumerable<CategoryResponse>>>[parallelCount];
-            const string connStr = "server=nil.fit; uid=test; pwd=/Ce55edoR6@nlb>B6Qw9; database=test; UseUsageAdvisor=true; CacheServerProperties=true; logging=true;";
+            const string connStr = "server=nil.fit; uid=test; pwd=/Ce55edoR6@nlb>B6Qw9; database=test; UseUsageAdvisor=true; CacheServerProperties=true; logging=true; MaximumPoolsize=200;";
             for (var i = 0; i < parallelCount; i++)
             {
                 var controller = new CategoryController(_logger);
